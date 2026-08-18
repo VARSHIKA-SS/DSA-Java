3dbd35a6e2dc36d1068e3319261d0f2fd3be7fe4
VARSHIKA-SS <varshika8172@gmail.com>
2026-08-18 16:31:56 +0530

    feat: add palindrome program in java

---
+++ DEFAULT/Palindromeno.java
@@ -0,0 +1,19 @@
+package DEFAULT;
+import java.util.*;
+public class Palindromeno {
+  public static boolean isNumberPalin(int no){
+    int rev=0;
+    int sum=no;
+    while(no>0){
+      rev*=10;
+      rev+=no%10;
+      no/=10;}
+    if (rev==sum) return true;
+    else return false;
+  }
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int m=sc.nextInt();
+    System.out.println(isNumberPalin(m));
+  }
+}
